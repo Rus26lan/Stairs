039a10dbbb4eda2022b9e1c5dc2ade7bbf729086
Rus26lan <rundgrun@gmail.com>
2023-05-26 16:20:12 +0300

    Add bottom navigation bar

--- app/src/main/java/com/rundgrun/stairs/ui/MainActivity.kt
+++ app/src/main/java/com/rundgrun/stairs/ui/MainActivity.kt
@@ -8,102 +8,26 @@ import android.view.GestureDetector
 import android.view.GestureDetector.OnGestureListener
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rundgrun.stairs.R
+import com.rundgrun.stairs.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), ScaleGestureDetector.OnScaleGestureListener,OnGestureListener {
+class MainActivity : AppCompatActivity() {
 
-    private var glSurfaceView: GLSurfaceView? = null
-    private var renderer: OpenGLRenderer? = null
-    private lateinit var gestureDetector: GestureDetector
-    private lateinit var scaleDetector: ScaleGestureDetector
+    private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("ClickableViewAccessibility")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        gestureDetector = GestureDetector(this,this)
-        scaleDetector = ScaleGestureDetector(this, this)
-        if (!supportES2()) {
-            Toast.makeText(this, "Not supported!", Toast.LENGTH_LONG).show()
-        }
-        glSurfaceView = GLSurfaceView(this)
-        renderer = OpenGLRenderer(this)
-        glSurfaceView?.setEGLContextClientVersion(2)
-        glSurfaceView?.setRenderer(renderer)
-        glSurfaceView?.setOnTouchListener { _, e ->
-            scaleDetector.onTouchEvent(e)
-            gestureDetector.onTouchEvent(e)
-            true
-        }
-        setContentView(glSurfaceView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
     }
 
-    override fun onPause() {
-        super.onPause()
-        glSurfaceView?.onPause()
-    }
 
-    override fun onResume() {
-        super.onResume()
-        glSurfaceView?.onResume()
-    }
-
-    private fun supportES2(): Boolean {
-        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
-        val configurationInfo = activityManager.deviceConfigurationInfo
-        return configurationInfo.reqGlEsVersion >= 0x20000
-    }
-
-    override fun onScale(detector: ScaleGestureDetector): Boolean {
-        val factor = detector.scaleFactor
-        if (factor > 1){
-            renderer?.scale(0.02f)
-        } else{
-            renderer?.scale(-0.02f)
-        }
-        return true
-    }
-
-    override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
-        return true
-    }
-
-    override fun onScaleEnd(detector: ScaleGestureDetector) {
-    }
-
-    override fun onDown(e: MotionEvent): Boolean {
-        return true
-    }
-
-    override fun onShowPress(e: MotionEvent) {
-    }
-
-    override fun onSingleTapUp(e: MotionEvent): Boolean {
-        return true
-    }
-
-    override fun onScroll(
-        e1: MotionEvent,
-        e2: MotionEvent,
-        distanceX: Float,
-        distanceY: Float
-    ): Boolean {
-        if(!scaleDetector.isInProgress){
-            renderer?.rotate(distanceX * 0.001f,distanceY* 0.001f)
-        }
-        return true
-    }
-
-    override fun onLongPress(e: MotionEvent) {
-
-    }
-
-    override fun onFling(
-        e1: MotionEvent,
-        e2: MotionEvent,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-        return true
-    }
 }
---
+++ app/src/main/java/com/rundgrun/stairs/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.rundgrun.stairs.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
